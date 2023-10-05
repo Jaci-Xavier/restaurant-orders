d4210737b914c0b6f157e3c714674d49d15411b4
Jaci-Xavier <devxvr@gmail.com>
2023-10-05 20:19:13 -0300

    refatorando o req 4 com função anonima (lambda), map e filter

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -26,18 +26,27 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
-        return [
-            {
-                'dish_name': dish.name,
-                'ingredients': dish.get_ingredients(),
-                'price': dish.price,
-                'restrictions': dish.get_restrictions()
-            }
-            for dish in self.menu_data.dishes
-            if (restriction not in dish.get_restrictions() and
-                all(ingredient in self.inventory.inventory 
-                    for ingredient in dish.get_ingredients()))
-        ]
+        return list(map(lambda dish: {
+            'dish_name': dish.name,
+            'ingredients': dish.get_ingredients(),
+            'price': dish.price,
+            'restrictions': dish.get_restrictions()
+         }, filter(lambda dish: restriction not in dish.get_restrictions()
+                   and all(ingredient in self.inventory.inventory
+                           for ingredient in dish
+                           .get_ingredients()), self.menu_data.dishes)))
+        # return [
+        #     {
+        #         'dish_name': dish.name,
+        #         'ingredients': dish.get_ingredients(),
+        #         'price': dish.price,
+        #         'restrictions': dish.get_restrictions()
+        #     }
+        #     for dish in self.menu_data.dishes
+        #     if (restriction not in dish.get_restrictions() and
+        #         all(ingredient in self.inventory.inventory 
+        #             for ingredient in dish.get_ingredients()))
+        # ]
         # menu = []
         # for dish in self.menu_data.dishes:
         #     if restriction in dish.get_restrictions():
